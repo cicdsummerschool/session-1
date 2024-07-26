01e7a5913dac86b330c429903e9f7fc5f58764b8
Michael <managedkaos@users.noreply.github.com>
2024-07-25 22:20:59 -0700

    updates for linting and testing

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 This is a Python script that prints "Hello, World!" to the console.
 """
 
+
 def hello_world():
     """
     This is where the magic happens
     """
-    print('Hello, World!')
+    print("Hello, World!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     hello_world()
